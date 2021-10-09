c18035f95fd6e4289bc49de06b90404e8d9badf4
Jakub <donicjakub@gmail.com>
2021-10-09 11:42:22 +0200

    Multiple clients, testing some functionalities.

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
-from fastapi import FastAPI, WebSocket
+from datetime import datetime, time, timedelta, timezone
+from typing import List, Optional
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Body
 from fastapi.responses import HTMLResponse
+from fastapi.testclient import TestClient
+
 
 app = FastAPI()
 
-#client side
+#client
+
 html = """
 <!DOCTYPE html>
 <html>
     <head>
         <style>
             body{
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
             }
         </style>
         <title>Chat</title>
     </head>
-    <body >
+    <body>
         <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
         <form action="" onsubmit="sendMessage(event)">
             <input type="text" id="messageText" autocomplete="off"/>
             <button>Send</button>
@@ -27,7 +33,9 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
             ws.onmessage = function(event) {
                 var messages = document.getElementById('messages')
                 var message = document.createElement('li')
@@ -46,16 +54,59 @@ html = """
 </html>
 """
 
+#server
 
-#server side
+current_date = datetime
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+user_name = "Kuba"
+
 @app.get("/")
 async def get():
     return HTMLResponse(html)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    print('Connecting...')
+    await manager.connect(websocket)
+    try:
+        while True: 
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"Text: {data}, added by: {user_name}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+
+    except WebSocketDisconnect:
+
+        manager.disconnect(websocket)
+
+
+        await manager.broadcast(f"Client #{client_id} disconnected!")
+        
+
+
+def test_websocket():
+    client = TestClient(app)
+    with client.websocket_connect("/ws") as websocket:
+        data = websocket.receive_json()
+        assert data == {"msg": "Hello WebSocket"}
