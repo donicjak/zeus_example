06cc21471fa60c774913a2005d9fa47ead11c2ed
Jakub <donicjakub@gmail.com>
2021-10-18 16:07:47 +0200

    Removing methods that were redundant from database_handler.py

--- database_handler.py
+++ database_handler.py
@@ -9,30 +9,10 @@ def get_client():
                               password="", host="localhost")
     return clickhouse_client
 
-
-def create_db():
-    clickhouse_client.execute('''
-                            CREATE TABLE eventlog (
-                                 message String,
-                                 timestamp DateTime)
-                            ENGINE = MergeTree()
-                            ORDER BY timestamp;
-                            ''')
-
-
 def insert_data(message):
     clickhouse_client = get_client()
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
- #   index = get_max_index() + 1
     clickhouse_client.execute(f"INSERT INTO eventlog VALUES( '{message}', '{now}')")
-
-
-#def get_max_index():
-  #  clickhouse_client = get_client()
-  #  max_index = clickhouse_client.execute("SELECT max(id) FROM eventlog")
-  #  max_index = int(max_index[0][0])
-   # return max_index
-
 
 def get_data_frame():
     clickhouse_client = get_client()
