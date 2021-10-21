7837e1b5efec0ced7c5ef54b9259111d95ec47c6
Jakub <donicjakub@gmail.com>
2021-10-21 18:27:39 +0200

    Fixing database handler with flake8

--- database_handler.py
+++ database_handler.py
@@ -1,10 +1,8 @@
-import json
 import time
 import os
 
 from typing import List, Dict, Any
 from clickhouse_driver import Client
-from datetime import datetime
 
 
 def get_client() -> Client:
@@ -12,24 +10,29 @@ def get_client() -> Client:
     user_name = os.getenv("user_name", "default")
     host_name = os.getenv("host_name", "localhost")
     clickhouse_client = Client(database=database_name, user=user_name,
-                              password="", host=host_name)
+                               password="", host=host_name)
     return clickhouse_client
 
-def insert_data(message: str, ip_address : int) -> None:
-    clickhouse_client  = get_client()
+
+def insert_data(message: str, ip_address: int) -> None:
+    clickhouse_client = get_client()
     now = int(time.time())
-    clickhouse_client.execute(f"INSERT INTO eventlog VALUES( '{message}', '{now}', '{ip_address}')")
+    clickhouse_client.execute(f"INSERT INTO eventlog VALUES('{message}',\
+                              '{now}', '{ip_address}')")
+
 
 def get_content() -> List:
     clickhouse_client = get_client()
     database_content = clickhouse_client.execute('SELECT * FROM eventlog')
     return database_content
-    
+
+
 def to_dictionary_list() -> List[Dict[str, Any]]:
     database_content = get_content()
     database_list = []
     for item in database_content:
         timestamp_item = int(time.mktime(item[1].timetuple()))
-        d_item = {"message" : item[0], "timestamp" : timestamp_item, "ip_address" : item[2]}
+        d_item = {"message": item[0], "timestamp": timestamp_item,
+                  "ip_address": item[2]}
         database_list.append(d_item)
     return database_list
